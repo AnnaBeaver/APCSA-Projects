4bd9a083c6a29b9033cf4e7258c9ded84e48d24c
Anna <42617944+AnnaBeaver@users.noreply.github.com>
2018-09-25 10:38:01 -0700

    Act 2 printing ball, but its not moving

--- Project 2 - Ball World/BallRunner.java
+++ Project 2 - Ball World/BallRunner.java
@@ -21,8 +21,8 @@ public class BallRunner
         }
     }
     
-    public static int findFreeBallBotIndex(BallBot[] ballBotArray){
-        int freeBallBotIndex = 0;
+    //public static int findFreeBallBotIndex(BallBot[] ballBotArray){
+        //int freeBallBotIndex = 0;
         /*
         boolean runLoop = true;
         int i = 0;
@@ -36,20 +36,37 @@ public class BallRunner
                 runLoop = false;
             }
             */
-    }
+          // return 0;
+    //}
     
     public static void activityTwo(){
-        BallWorld bw = new BallWorld(500,500);
-        TGPoint entrancePoint = new TGPoint(0,0); 
+        BallWorld ballWorld = new BallWorld(500,500);
+        BallBot ballBot = new BallBot(ballWorld,new TGPoint(0,0),0,25);
+        //BallWorld bw = new BallWorld(500,500);
+        //TGPoint entrancePoint = new TGPoint(0,0); 
         BallBot[] ballBotArray = new BallBot[10];
         BallRunner ballRunner = new BallRunner();
         boolean GenerateBalls = true;
-        
+        int freeBallBotIndex = 0;
         //int numofBalls = 0;
         boolean runLoop = true;
-        while (runloop == true){
-            if (){
-                
+        while (true){
+            /*if (ballBot.canMoveForward(ballWorld) == true){
+                ballBot.moveForward();
+            }
+            if (ballBot.canMoveForward(ballWorld) == false){
+                ballBot.setHeading(ballBot.getHeading() + 35);
+            }
+            */
+            if (freeBallBotIndex < ballBotArray.length){
+               //if there is room in BallBot Array for an addition to BallBot
+               BallBot ballBot2 = new BallBot(ballWorld,new TGPoint(0,0),10,25); 
+            }
+            if (ballBot.canMoveForward(ballWorld) == true){
+                ballBot.moveForward();
+            }
+            if (ballBot.canMoveForward(ballWorld) == false){
+                ballBot.setHeading(ballBot.getHeading() + 35);
             }
         }
     }
