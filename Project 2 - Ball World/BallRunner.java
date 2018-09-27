1c10227d798488f7423ff38d53b0bea1878f0629
Anna <42617944+AnnaBeaver@users.noreply.github.com>
2018-09-27 10:34:18 -0700

    2 balls bouncing
    help from guy to find the problems

--- Project 2 - Ball World/BallRunner.java
+++ Project 2 - Ball World/BallRunner.java
@@ -20,67 +20,70 @@ public class BallRunner
             }
         }
     }
-    
-    //public static int findFreeBallBotIndex(BallBot[] ballBotArray){
+
+    public static int findFreeBallBotIndex(BallBot[] ballBotArray){
         //int freeBallBotIndex = 0;
-        /*
-        boolean runLoop = true;
-        int i = 0;
-        int returnValue = -3;
-        while(runLoop == true){
-            if (ballBotArray[i] == null){
-                runLoop = false;
-                returnValue = i;
-            }
-            if(i == ballBotArray.length-1){
-                runLoop = false;
-            }
-            */
-          // return 0;
-    //}
+
+        //boolean runLoop = true;
+        int returnValue = 0;
+        for(int i = 0; i < ballBotArray.length; i++){
+                if (ballBotArray[i] == null){
     
+                    returnValue = i;
+                }
+                
+               // return returnValue;
+            
+        }
+        if(returnValue == 0){
+            returnValue = ballBotArray.length;
+        }
+        return returnValue;
+    }
+
     public static void activityTwo(){
         BallWorld ballWorld = new BallWorld(500,500);
-        BallBot ballBot = new BallBot(ballWorld,new TGPoint(0,0),0,25);
+        //BallBot ballBot = new BallBot(ballWorld,new TGPoint(0,0),0,25);
         //BallWorld bw = new BallWorld(500,500);
-        //TGPoint entrancePoint = new TGPoint(0,0); 
-        BallBot[] ballBotArray = new BallBot[10];
+        TGPoint entrancePoint = new TGPoint(0,0); 
+        BallBot[] ballBotArray = new BallBot[3];
         BallRunner ballRunner = new BallRunner();
-        boolean GenerateBalls = true;
-        int freeBallBotIndex = 0;
-        //int numofBalls = 0;
-        boolean runLoop = true;
+        //int freeBallBotIndex = 0;
+        //int numofBalls = 0; 
         while (true){
             /*if (ballBot.canMoveForward(ballWorld) == true){
-                ballBot.moveForward();
+            ballBot.moveForward();
             }
             if (ballBot.canMoveForward(ballWorld) == false){
-                ballBot.setHeading(ballBot.getHeading() + 35);
+            ballBot.setHeading(ballBot.getHeading() + 35);
             }
-            */
-            if (freeBallBotIndex < ballBotArray.length){
-               //if there is room in BallBot Array for an addition to BallBot
-               BallBot ballBot2 = new BallBot(ballWorld,new TGPoint(0,0),10,25); 
+             */
+            int n = ballRunner.findFreeBallBotIndex(ballBotArray);
+            if (n < ballBotArray.length){
+                //if there is room in BallBot Array for an addition to BallBot
+                ballBotArray[n] = new BallBot(ballWorld,entrancePoint,(Math.random()*360),25);
             }
-            if (ballBot.canMoveForward(ballWorld) == true){
-                ballBot.moveForward();
-            }
-            if (ballBot.canMoveForward(ballWorld) == false){
-                ballBot.setHeading(ballBot.getHeading() + 35);
-            }
+            for(int a = 0; a < ballBotArray.length; a++){
+                if(ballBotArray[a] != null){
+            
+            if (ballBotArray[a].canMoveForward(ballWorld)){
+                ballBotArray[a].moveForward();
+            }else{
+                ballBotArray[a].setHeading(ballBotArray[a].getHeading() + 35);
+            }}
         }
     }
-}
+}}
 
 
 /**
  * if (GenerateBalls){
-                if(numofBalls == 0){
-                    
-                    if (findEntranceClear (BallBotArray,new TGPoint(0,0))){
-                        int bbindex = findFreeBallBoxIndex(BallBotArray);
-                        
-                    }
-                }
-            }
+if(numofBalls == 0){
+
+if (findEntranceClear (BallBotArray,new TGPoint(0,0))){
+int bbindex = findFreeBallBoxIndex(BallBotArray);
+
+}
+}
+}
  */
