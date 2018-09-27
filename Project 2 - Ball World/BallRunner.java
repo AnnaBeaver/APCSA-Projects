56e62190f4c8fe3223249de77558746b02171b6c
Anna <42617944+AnnaBeaver@users.noreply.github.com>
2018-09-27 10:35:52 -0700

    Code claned up, 2 balls bouncing

--- Project 2 - Ball World/BallRunner.java
+++ Project 2 - Ball World/BallRunner.java
@@ -22,18 +22,12 @@ public class BallRunner
     }
 
     public static int findFreeBallBotIndex(BallBot[] ballBotArray){
-        //int freeBallBotIndex = 0;
-
-        //boolean runLoop = true;
         int returnValue = 0;
         for(int i = 0; i < ballBotArray.length; i++){
-                if (ballBotArray[i] == null){
-    
-                    returnValue = i;
-                }
-                
-               // return returnValue;
-            
+            if (ballBotArray[i] == null){
+
+                returnValue = i;
+            }
         }
         if(returnValue == 0){
             returnValue = ballBotArray.length;
@@ -43,21 +37,10 @@ public class BallRunner
 
     public static void activityTwo(){
         BallWorld ballWorld = new BallWorld(500,500);
-        //BallBot ballBot = new BallBot(ballWorld,new TGPoint(0,0),0,25);
-        //BallWorld bw = new BallWorld(500,500);
         TGPoint entrancePoint = new TGPoint(0,0); 
         BallBot[] ballBotArray = new BallBot[3];
         BallRunner ballRunner = new BallRunner();
-        //int freeBallBotIndex = 0;
-        //int numofBalls = 0; 
         while (true){
-            /*if (ballBot.canMoveForward(ballWorld) == true){
-            ballBot.moveForward();
-            }
-            if (ballBot.canMoveForward(ballWorld) == false){
-            ballBot.setHeading(ballBot.getHeading() + 35);
-            }
-             */
             int n = ballRunner.findFreeBallBotIndex(ballBotArray);
             if (n < ballBotArray.length){
                 //if there is room in BallBot Array for an addition to BallBot
@@ -65,16 +48,16 @@ public class BallRunner
             }
             for(int a = 0; a < ballBotArray.length; a++){
                 if(ballBotArray[a] != null){
-            
-            if (ballBotArray[a].canMoveForward(ballWorld)){
-                ballBotArray[a].moveForward();
-            }else{
-                ballBotArray[a].setHeading(ballBotArray[a].getHeading() + 35);
-            }}
-        }
-    }
-}}
 
+                    if (ballBotArray[a].canMoveForward(ballWorld)){
+                        ballBotArray[a].moveForward();
+                    }else{
+                        ballBotArray[a].setHeading(ballBotArray[a].getHeading() + 35);
+                    }
+                }
+            }
+        }
+    }}
 
 /**
  * if (GenerateBalls){
