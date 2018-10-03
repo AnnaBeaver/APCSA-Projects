aadc65a51183d98d7bb39fd0bb8d92931e5b8132
Anna <AnnaBeaver@users.noreply.github.com>
2018-10-03 10:18:42 -0700

    Act 3 working, 4 has just been copied

--- Project 2 - Ball World/BallRunner.java
+++ Project 2 - Ball World/BallRunner.java
@@ -21,6 +21,7 @@ public class BallRunner
         }
     }
 
+    
     public static int findFreeBallBotIndex(BallBot[] ballBotArray){
         int returnValue = 0;
         for(int i = 0; i < ballBotArray.length; i++){
@@ -38,7 +39,7 @@ public class BallRunner
     public static void activityTwo(){
         BallWorld ballWorld = new BallWorld(500,500);
         TGPoint entrancePoint = new TGPoint(0,0); 
-        BallBot[] ballBotArray = new BallBot[3];
+        BallBot[] ballBotArray = new BallBot[10];
         BallRunner ballRunner = new BallRunner();
         while (true){
             int n = ballRunner.findFreeBallBotIndex(ballBotArray);
@@ -58,40 +59,52 @@ public class BallRunner
         }
     }
 
+    
+    //find the distance between the ball about to be created and the ball made before that
     public double distanceBetweenPoints(TGPoint point1,TGPoint point2){
         double ReturnValue = 0.0;
         ReturnValue = Math.sqrt(Math.pow(point1.x-point2.x,2) + (Math.pow(point1.y-point2.y,2)));
         return ReturnValue;
     }
 
-    
+    //Make sure that the ball is far enough away to make a new one
     public boolean enteranceClear(BallBot[] ballBotArray, TGPoint enterancePoint){
         BallRunner ballRunner = new BallRunner();
-        boolean Return = false;
+        //boolean Return = true;
+        //traverse the array
         for(int i = 0; i < ballBotArray.length; i++){
-            if (ballBotArray[i] == null){
-                Return = false;
-            }
-            if(Return == true){
-                if (ballRunner.distanceBetweenPoints(ballBotArray[i].getPoint(),enterancePoint ) > 100){
-                   Return = false;
+            //test if the spot's null
+            if (ballBotArray[i] != null){
+                //Return = false;
+
+                //if the spot is null, check to seee if the other ball is far enough away to make a new one
+                //if(Return == true){
+                if (ballRunner.distanceBetweenPoints(ballBotArray[i].getPoint(),enterancePoint ) < 100){
+                    return false;
                 }
+                //}
             }
         }
-        return Return;
+        //returns the boolean (true if the ball is far enough away, false if the ball is too close)
+        return true;
     }
 
     public static void activityThree(){
+        //Declare the world, enterance point, array length, and runner(which lets me refer to the other functions)
         BallWorld ballWorld = new BallWorld(500,500);
         TGPoint entrancePoint = new TGPoint(0,0); 
-        BallBot[] ballBotArray = new BallBot[3];
+        BallBot[] ballBotArray = new BallBot[10];
         BallRunner ballRunner = new BallRunner();
-        while (ballRunner.enteranceClear(ballBotArray,entrancePoint) == true){
-            int n = ballRunner.findFreeBallBotIndex(ballBotArray);
-            if (n < ballBotArray.length){
-                //if there is room in BallBot Array for an addition to BallBot
-                ballBotArray[n] = new BallBot(ballWorld,entrancePoint,(Math.random()*360),25);
+        while( true){
+            //if the ball is far enough away, make a new ball
+            if (ballRunner.enteranceClear(ballBotArray,entrancePoint) == true){
+                int n = ballRunner.findFreeBallBotIndex(ballBotArray);
+                if (n < ballBotArray.length){
+                    //if there is room in BallBot Array for an addition to BallBot
+                    ballBotArray[n] = new BallBot(ballWorld,entrancePoint,(Math.random()*360),25);
+                }
             }
+            //TRAVERSE ARRAY and make the new ball move
             for(int a = 0; a < ballBotArray.length; a++){
                 if(ballBotArray[a] != null){
                     if (ballBotArray[a].canMoveForward(ballWorld)){
@@ -101,19 +114,68 @@ public class BallRunner
                     }
                 }
             }
+
         }
-        
+    }
+    
+    
+    
+    /**
+       //find the distance between the ball about to be created and the ball made before that
+    public double distanceBetweenPoints(TGPoint point1,TGPoint point2){
+        double ReturnValue = 0.0;
+        ReturnValue = Math.sqrt(Math.pow(point1.x-point2.x,2) + (Math.pow(point1.y-point2.y,2)));
+        return ReturnValue;
+    }
+
+    //Make sure that the ball is far enough away to make a new one
+    public boolean enteranceClear(BallBot[] ballBotArray, TGPoint enterancePoint){
+        BallRunner ballRunner = new BallRunner();
+        //boolean Return = true;
+        //traverse the array
+        for(int i = 0; i < ballBotArray.length; i++){
+            //test if the spot's null
+            if (ballBotArray[i] != null){
+                //Return = false;
+
+                //if the spot is null, check to seee if the other ball is far enough away to make a new one
+                //if(Return == true){
+                if (ballRunner.distanceBetweenPoints(ballBotArray[i].getPoint(),enterancePoint ) < 100){
+                    return false;
+                }
+                //}
+            }
+        }
+        //returns the boolean (true if the ball is far enough away, false if the ball is too close)
+        return true;
+    }*/
+
+    public static void activityFour(){
+        //Declare the world, enterance point, array length, and runner(which lets me refer to the other functions)
+        BallWorld ballWorld = new BallWorld(500,500);
+        TGPoint entrancePoint = new TGPoint(0,0); 
+        BallBot[] ballBotArray = new BallBot[10];
+        BallRunner ballRunner = new BallRunner();
+        while( true){
+            //if the ball is far enough away, make a new ball
+            if (ballRunner.enteranceClear(ballBotArray,entrancePoint) == true){
+                int n = ballRunner.findFreeBallBotIndex(ballBotArray);
+                if (n < ballBotArray.length){
+                    //if there is room in BallBot Array for an addition to BallBot
+                    ballBotArray[n] = new BallBot(ballWorld,entrancePoint,(Math.random()*360),25);
+                }
+            }
+            //TRAVERSE ARRAY and make the new ball move
+            for(int a = 0; a < ballBotArray.length; a++){
+                if(ballBotArray[a] != null){
+                    if (ballBotArray[a].canMoveForward(ballWorld)){
+                        ballBotArray[a].moveForward();
+                    }else{
+                        ballBotArray[a].setHeading(ballBotArray[a].getHeading() + 35);
+                    }
+                }
+            }
+
+        }
     }
 }
-
-/**
- * if (GenerateBalls){
-if(numofBalls == 0){
-
-if (findEntranceClear (BallBotArray,new TGPoint(0,0))){
-int bbindex = findFreeBallBoxIndex(BallBotArray);
-
-}
-}
-}
- */
