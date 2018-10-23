ab127e8ffbfaa9afeef7117494d906a4fb316851
Anna <AnnaBeaver@users.noreply.github.com>
2018-10-23 09:37:31 -0700

    Act 3 working

--- Project3 - MagPie/MagpieActivityStarterCode/activity3/StringExplorer.java
+++ Project3 - MagPie/MagpieActivityStarterCode/activity3/StringExplorer.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * A program to allow students to try out different 
  * String methods. 
@@ -26,32 +25,3 @@ public class StringExplorer
 	}
 
 }
-=======
-/**
- * A program to allow students to try out different 
- * String methods. 
- * @author Laurie White
- * @version April 2012
- */
-public class StringExplorer
-{
-
-	public static void main(String[] args)
-	{
-		String sample = "The quick brown fox jumped over the lazy dog.";
-		
-		//  Demonstrate the indexOf method.
-		int position = sample.indexOf("quick");
-		System.out.println ("sample.indexOf(\"quick\") = " + position);
-		
-		//  Demonstrate the toLowerCase method.
-		String lowerCase = sample.toLowerCase();
-		System.out.println ("sample.toLowerCase() = " + lowerCase);
-		System.out.println ("After toLowerCase(), sample = " + sample);
-		
-		//  Try other methods here:
-
-	}
-
-}
->>>>>>> 75a76530cea06f81e6fe56e9dc77afb39db7d760
