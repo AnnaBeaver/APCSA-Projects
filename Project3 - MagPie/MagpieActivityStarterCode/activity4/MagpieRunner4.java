a2b3fdad7bac678cd01b539cebc0ca1f7efeecc8
Anna <AnnaBeaver@users.noreply.github.com>
2018-10-23 10:06:03 -0700

    All Activities done

--- Project3 - MagPie/MagpieActivityStarterCode/activity4/MagpieRunner4.java
+++ Project3 - MagPie/MagpieActivityStarterCode/activity4/MagpieRunner4.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import java.util.Scanner;
 
 /**
@@ -28,34 +28,3 @@ public class MagpieRunner4
 	}
 
 }
-=======
-import java.util.Scanner;
-
-/**
- * A simple class to run the Magpie class.
- * @author Laurie White
- * @version April 2012
- */
-public class MagpieRunner4
-{
-
-	/**
-	 * Create a Magpie, give it user input, and print its replies.
-	 */
-	public static void main(String[] args)
-	{
-		Magpie4 maggie = new Magpie4();
-		
-		System.out.println (maggie.getGreeting());
-		Scanner in = new Scanner (System.in);
-		String statement = in.nextLine();
-		
-		while (!statement.equals("Bye"))
-		{
-			System.out.println (maggie.getResponse(statement));
-			statement = in.nextLine();
-		}
-	}
-
-}
->>>>>>> 75a76530cea06f81e6fe56e9dc77afb39db7d760
